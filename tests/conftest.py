fed8a51f0d29eb282a2ea7b6565efb42e178b8fe
anshika1179 <anshikarai459@gmail.com>
2026-06-24 14:34:57 +0530

    Add src to sys.path in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,9 @@ import sys
 
 # Add the project root to sys.path so that 'app' module can be imported by tests
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+src_path = os.path.join(project_root, "src")
+
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
+if src_path not in sys.path:
+    sys.path.insert(0, src_path)
